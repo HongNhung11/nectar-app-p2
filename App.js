cdb0b439490211668d40bb22ad316a5713ff1eb1
HongNhung11 <nguyenthinhung072005@gmail.com>
2026-03-29 10:21:47 +0700

    Change initial route from 'Explore' to 'Home'

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Import các màn hình của bạn
+// Import đầy đủ các màn hình
+import HomeScreen from './screens/HomeScreen'; 
 import ExploreScreen from './screens/Explore'; 
 import BeveragesScreen from './screens/Beverages'; 
+import ProductDetailScreen from './screens/ProductDetail';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Explore">
+      <Stack.Navigator initialRouteName="Home">
+        
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ headerShown: false }} 
+        />
+
         <Stack.Screen 
           name="Explore" 
           component={ExploreScreen} 
           options={{ headerShown: false }} 
         />
- 
+
         <Stack.Screen 
           name="Beverages" 
           component={BeveragesScreen} 
           options={{ headerShown: false }} 
         />
+
+        <Stack.Screen 
+          name="ProductDetail" 
+          component={ProductDetailScreen} 
+          options={{ headerShown: false }} 
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
